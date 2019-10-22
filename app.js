bd1706f65b66e93225841ecc57d6081ad2029cb7
hawrot <mhawro200@caledonian.ac.uk>
2019-10-22 13:03:58 +0100

    Create simple functionality with vanillaJs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const reasonInput = document.querySelector('#input-reason');
 const amount = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-clear');
 const confirmBtn = document.querySelector('#btn-add-expense');
+const expensesList = document.querySelector('#expenses-list');
 
 let list = [];
 
@@ -12,7 +13,7 @@ confirmBtn.addEventListener('click', () =>{
     if (enteredReason.trim().length <=0 || enterendAmount <=0 || enterendAmount.trim().length <=0){
         return;
     }
-    console.log(enteredReason, '£', enterendAmount);
-    list.push({value: enteredReason, amount: enterendAmount});
-    console.log(list);
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': £' + enterendAmount;
+    expensesList.appendChild(newItem);
 });
