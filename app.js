f99c9d647d5afbb27f9fb08e2d7be138f31ab04b
hawrot <mhawro200@caledonian.ac.uk>
2019-10-22 13:17:05 +0100

    Add native alert

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const amount = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-clear');
 const confirmBtn = document.querySelector('#btn-add-expense');
 const expensesList = document.querySelector('#expenses-list');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let list = [];
 
@@ -16,6 +17,13 @@ confirmBtn.addEventListener('click', () =>{
     const enterendAmount = amount.value;
 
     if (enteredReason.trim().length <=0 || enterendAmount <=0 || enterendAmount.trim().length <=0){
+        alertCtrl.create({
+            message: 'Enter valid data',
+            header: 'invalid input',
+            buttons: ['Ok']
+        }).then(alertElement =>{
+            alertElement.present();
+        });
         return;
     }
     clear();
