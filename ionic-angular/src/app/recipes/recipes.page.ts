6631cacbd4b48566eaad8223169ec5070d92008f
hawrot <mhawro200@caledonian.ac.uk>
2020-02-20 11:41:17 +0000

    Use of ionViewWillEnter/DidEnter,WillLeave, DidLeave

--- ionic-angular/src/app/recipes/recipes.page.ts
+++ ionic-angular/src/app/recipes/recipes.page.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {RecipesService} from './recipes.service';
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.page.html',
-  styleUrls: ['./recipes.page.scss'],
+    selector: 'app-recipes',
+    templateUrl: './recipes.page.html',
+    styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
 
-  recipes: Recipe[];
+    recipes: Recipe[];
 
-  constructor(private recipesService: RecipesService) { }
+    constructor(private recipesService: RecipesService) {
+    }
 
-  ngOnInit() {
-   this. recipes = this.recipesService.getAppRecipes();
-  }
+    ngOnInit() {
+
+    }
+
+    ionViewWillEnter() {
+        this.recipes = this.recipesService.getAppRecipes();
+        console.log('ionViewWillEnter');
+    }
+
+    ionViewDidEnter() {
+        console.log('ionViewDidEnter');
+    }
+
+    ionViewWillLeave() {
+        console.log('ionViewWILLLeave');
+    }
+
+    ionViewDidLeave() {
+        console.log('ionViewDidLeave');
+    }
+
+    ngOnDestroy(): void {
+    }
 
 }
