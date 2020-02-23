bd6bae7d94d7a9585d600513ac8c9d726cf5b681
hawrot <mhawro200@caledonian.ac.uk>
2020-02-23 20:54:43 +0000

    Add simple authentication

--- ionic-angular/src/app/auth/auth.guard.ts
+++ ionic-angular/src/app/auth/auth.guard.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  CanActivate,
+  CanActivateChild,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -19,6 +34,9 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    if(!this.authService._userIsAuthenticated){
+      this.router.navigateByUrl('/auth');
+    }
+    return this.authService._userIsAuthenticated;
   }
 }
--- ionic-angular/src/app/auth/auth.service.ts
+++ ionic-angular/src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import {Injectable} from '@angular/core';
     providedIn: 'root'
 })
 export class AuthService {
-    private _userIsAuthenticated = false;
+    _userIsAuthenticated = false;
 
     constructor() {
     }
