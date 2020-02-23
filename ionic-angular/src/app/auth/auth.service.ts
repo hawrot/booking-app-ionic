0348b4d021b21dcfc98bcbc2c155a24c23f261bd
hawrot <mhawro200@caledonian.ac.uk>
2020-02-23 20:38:40 +0000

    Add guard

--- ionic-angular/src/app/auth/auth.service.ts
+++ ionic-angular/src/app/auth/auth.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-userIsAuthenticated = false;
-  constructor() { }
+    private _userIsAuthenticated = false;
 
-  login(){
-  this.userIsAuthenticated = true;
-  }
-  logout(){
-    this.userIsAuthenticated = false;
-  }
+    constructor() {
+    }
+
+    login() {
+        this._userIsAuthenticated = true;
+    }
+
+    logout() {
+        this._userIsAuthenticated = false;
+    }
 }
