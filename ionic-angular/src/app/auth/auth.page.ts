39a75da9a17f70619d87845b9f39ae8a61f7a907
hawrot <mhawro200@caledonian.ac.uk>
2020-02-24 23:22:33 +0000

    Add a spinner

--- ionic-angular/src/app/auth/auth.page.ts
+++ ionic-angular/src/app/auth/auth.page.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+    selector: 'app-auth',
+    templateUrl: './auth.page.html',
+    styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+    isLoading = false;
 
-  ngOnInit() {
-  }
+    constructor(private authService: AuthService, private router: Router) {
+    }
 
- onLogin(){
-    this.authService.login();
-    this.router.navigateByUrl('/places/tabs/discover');
- }
+    ngOnInit() {
+    }
+
+    onLogin() {
+        this.isLoading = true;
+        this.authService.login();
+        setTimeout(() => {
+            this.isLoading = false;
+            this.router.navigateByUrl('/places/tabs/discover');
+        }, 1500)
+
+    }
 }
