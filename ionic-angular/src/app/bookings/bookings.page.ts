f6ae850616e535103b96ed2fdd498b6fcfdba7ce
hawrot <mhawro200@caledonian.ac.uk>
2020-02-24 22:36:28 +0000

    Add service and model for bookings

--- ionic-angular/src/app/bookings/bookings.page.ts
+++ ionic-angular/src/app/bookings/bookings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BookingService} from './booking.service';
+import {Booking} from './booking.model';
 
 @Component({
   selector: 'app-bookings',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsPage implements OnInit {
 
-  constructor() { }
+  loadedBookings: Booking[];
+
+  constructor(private bookingService: BookingService) { }
 
   ngOnInit() {
+    this.loadedBookings = this.bookingService.bookings;
   }
 
 }
