55194967047a207d8c576a160bd48fe9a1f38996
hawrot <mhawro200@caledonian.ac.uk>
2020-02-25 12:34:30 +0000

    Assign the color to the toolbar by the use of variables.scss

--- ionic-angular/src/app/auth/auth.service.ts
+++ ionic-angular/src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import {Injectable} from '@angular/core';
     providedIn: 'root'
 })
 export class AuthService {
-    _userIsAuthenticated = false;
+    _userIsAuthenticated = true;
 
     constructor() {
     }
