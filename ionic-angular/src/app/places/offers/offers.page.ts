ee5f04a292d1d1857fba050369ce96c59df8ef78
hawrot <mhawro200@caledonian.ac.uk>
2020-02-25 13:23:31 +0000

    Add custom css to style the text

--- ionic-angular/src/app/places/offers/offers.page.ts
+++ ionic-angular/src/app/places/offers/offers.page.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {PlacesService} from '../places.service';
-import {Place} from '../place.model';
-import {of} from 'rxjs';
-import {IonItemSliding} from '@ionic/angular';
-import {Router} from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+  styleUrls: ['./offers.page.scss']
 })
 export class OffersPage implements OnInit {
-offers: Place[];
-  constructor(private placesService: PlacesService, private router: Router){ }
+  offers: Place[];
+
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.placesService.places;
   }
-  onEdit(offerId : string, slidingItem: IonItemSliding ){
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
     slidingItem.close();
     this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
-    console.log('Edit: ' + offerId);
+    console.log('Editing item', offerId);
   }
 }
