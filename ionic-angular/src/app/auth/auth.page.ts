54fed0ec82ceacde920541a8b8835b9956592ab5
hawrot <mhawro200@caledonian.ac.uk>
2020-02-27 11:44:03 +0000

    Add auth form

--- ionic-angular/src/app/auth/auth.page.ts
+++ ionic-angular/src/app/auth/auth.page.ts
@@ -35,7 +35,18 @@ export class AuthPage implements OnInit {
     }
 
     onSubmit(form: NgForm){
-        console.log(form);
+        if(!form.valid){
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        if(this.isLogin){
+            //Send a request to login servers
+
+        }else{
+            //Send a request to signup servers
+        }
     }
 
     onSwitchAuthMode(){
