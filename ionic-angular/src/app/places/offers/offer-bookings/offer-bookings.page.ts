9a708311469da8f8ed1a359eccc9852f5e0881c7
hawrot <mhawro200@caledonian.ac.uk>
2020-02-28 12:12:13 +0000

    Added subscription to the places in offer-bookings.page.ts

--- ionic-angular/src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ ionic-angular/src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Place} from '../../place.model';
 import {ActivatedRoute} from '@angular/router';
 import {NavController} from '@ionic/angular';
 import {PlacesService} from '../../places.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-offer-bookings',
   templateUrl: './offer-bookings.page.html',
   styleUrls: ['./offer-bookings.page.scss'],
 })
-export class OfferBookingsPage implements OnInit {
+export class OfferBookingsPage implements OnInit, OnDestroy {
 place: Place;
+private placeSub : Subscription;
 
   constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
 
@@ -20,8 +22,16 @@ place: Place;
         this.navCtrl.navigateBack('/places/tabs/offers');
         return;
       }
-      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      this.placeSub = this.placesService.getPlace(paramMap.get('placeId')).subscribe(place =>{
+        this.place = place;
+      });
     });
+  }
+
+  ngOnDestroy() {
+    if(this.placeSub){
+      this.placeSub.unsubscribe();
+    }
   }
 
 }
