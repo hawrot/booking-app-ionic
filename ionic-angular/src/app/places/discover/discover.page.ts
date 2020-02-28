a00fcbce056a9aca0514b830e34c96d106d103a1
hawrot <mhawro200@caledonian.ac.uk>
2020-02-28 12:03:22 +0000

    Added subscription to the places in discover.page.ts

--- ionic-angular/src/app/places/discover/discover.page.ts
+++ ionic-angular/src/app/places/discover/discover.page.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 import { SegmentChangeEventDetail } from '@ionic/core';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
-export class DiscoverPage implements OnInit {
+export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  private placesSub: Subscription;
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
-    this.loadedPlaces = this.placesService.places;
-    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+   // this.loadedPlaces = this.placesService.places;
+    this.placesSub = this.placesService.places.subscribe(places =>{
+      this.loadedPlaces = places;
+      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+    });
   }
 
   onEdit(offerId : string){
@@ -25,6 +30,12 @@ export class DiscoverPage implements OnInit {
   }
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>){
     console.log(event.detail);
+  }
+
+  ngOnDestroy() {
+    if(this.placesSub){
+      this.placesSub.unsubscribe();
+    }
   }
 
 }
