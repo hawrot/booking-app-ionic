07d36743954b5b84c698e692a2a16d69a5dfe835
hawrot <mhawro200@caledonian.ac.uk>
2020-02-28 12:32:16 +0000

    Add spinner for creating an offer (if delay occur)

--- ionic-angular/src/app/places/offers/new-offer/new-offer.page.ts
+++ ionic-angular/src/app/places/offers/new-offer/new-offer.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {PlacesService} from '../../places.service';
 import {Router} from '@angular/router';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
   selector: 'app-new-offer',
@@ -11,7 +12,7 @@ import {Router} from '@angular/router';
 export class NewOfferPage implements OnInit {
   form: FormGroup;
 
-  constructor(private placeServ: PlacesService, private router: Router) { }
+  constructor(private placeServ: PlacesService, private router: Router, private loadingController: LoadingController) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -27,9 +28,17 @@ export class NewOfferPage implements OnInit {
     if(!this.form.valid){
       return;
     }
-    this.placeServ.addPlace(this.form.value.title, this.form.value.description, +this.form.value.price, new Date(this.form.value.dateFrom), new Date(this.form.value.dateTo));
-    this.form.reset();
-    this.router.navigate(['/places/tabs/offers']);
+    this.loadingController.create({
+      message: 'Creating place...'
+    }).then(loadingEl =>{
+      loadingEl.present();
+      this.placeServ.addPlace(this.form.value.title, this.form.value.description, +this.form.value.price, new Date(this.form.value.dateFrom), new Date(this.form.value.dateTo)).subscribe(()=>{
+        loadingEl.dismiss();
+        this.form.reset();
+        this.router.navigate(['/places/tabs/offers']);
+      })
+    });
+
 
   }
 
--- ionic-angular/src/app/places/places.service.ts
+++ ionic-angular/src/app/places/places.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 import {Place} from './place.model';
 import {AuthService} from '../auth/auth.service';
 import {BehaviorSubject} from 'rxjs';
-import {map, take} from 'rxjs/operators';
+import {delay, map, take, tap} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -78,9 +78,8 @@ export class PlacesService {
         //this._places.push(newPlace);
 
         //Method below takes one object from the subscription
-        this._places.pipe(take(1)).subscribe(places => {
-            this._places.next(places.concat(newPlace));
-        })
-
+       return  this.places.pipe(take(1), tap(places =>{
+           this._places.next(places.concat(newPlace));
+       }))
     }
 }
