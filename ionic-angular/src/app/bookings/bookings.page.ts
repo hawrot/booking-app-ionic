caa1636d903e3684ba4c9b49ee1bb60bc9dd8b51
hawrot <mhawro200@caledonian.ac.uk>
2020-02-28 14:02:51 +0000

    Create spinner for booking cancelation - bookings.page.ts

--- ionic-angular/src/app/bookings/bookings.page.ts
+++ ionic-angular/src/app/bookings/bookings.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { IonItemSliding } from '@ionic/angular';
+import {IonItemSliding, LoadingController} from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 import { BookingService } from './booking.service';
@@ -14,7 +14,7 @@ export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
   private bookingSub: Subscription;
 
-  constructor(private bookingService: BookingService) {}
+  constructor(private bookingService: BookingService, private loadingController: LoadingController) {}
 
   ngOnInit() {
    this.bookingSub = this.bookingService.bookings.subscribe(bookings => {
@@ -22,8 +22,17 @@ export class BookingsPage implements OnInit, OnDestroy {
     });
   }
 
-  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+  onCancelBooking(bookingId: string, slidingEl: IonItemSliding) {
     slidingEl.close();
+    this.loadingController.create({
+      message: 'Canceling the booking...'
+    }).then(loadingEl =>{
+      loadingEl.present();
+      this.bookingService.cancelBooking(bookingId).subscribe(()=>{
+        loadingEl.dismiss();
+      });
+    })
+
     // cancel booking wiht id offerId
   }
 
