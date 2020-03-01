ee59da608ad05c41529eb8468d96d9ef47db1049
hawrot <mhawro200@caledonian.ac.uk>
2020-03-01 23:21:41 +0000

    Loading data in Discover Page

--- ionic-angular/src/app/places/discover/discover.page.ts
+++ ionic-angular/src/app/places/discover/discover.page.ts
@@ -16,6 +16,7 @@ export class DiscoverPage implements OnInit, OnDestroy {
   listedLoadedPlaces: Place[];
   relevantPlaces: Place[];
   private placesSub: Subscription;
+  isLoading = false;
 
   constructor(private placesService: PlacesService, private authService: AuthService) { }
 
@@ -39,6 +40,13 @@ export class DiscoverPage implements OnInit, OnDestroy {
       this.relevantPlaces = this.loadedPlaces.filter(place => place.userId !== this.authService.userId);
       this.listedLoadedPlaces = this.relevantPlaces.slice(1);
     }
+  }
+
+  ionViewWillEnter(){
+    this.isLoading = true;
+    this.placesService.fetchPlaces().subscribe(()=>{
+      this.isLoading = false;
+    });
   }
 
   ngOnDestroy() {
