3745876735dde650c2cd8cce0fcaeccccf243398
hawrot <mhawro200@caledonian.ac.uk>
2020-03-01 22:49:33 +0000

    Add spinner if loading data from Firebase

--- ionic-angular/src/app/places/offers/offers.page.ts
+++ ionic-angular/src/app/places/offers/offers.page.ts
@@ -14,6 +14,7 @@ import {Subscription} from 'rxjs';
 export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
   private placesSub: Subscription;
+  isLoading = false;
 
   constructor(private placesService: PlacesService, private router: Router) {}
 
@@ -31,7 +32,10 @@ export class OffersPage implements OnInit, OnDestroy {
   }
 
   ionViewWillEnter(){
-    this.placesService.fetchPlaces().subscribe();
+    this.isLoading = true;
+    this.placesService.fetchPlaces().subscribe(()=>{
+      this.isLoading = false;
+    });
   }
 
 
