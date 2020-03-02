a6a441c57507c169898034528517f88b2f19ab99
hawrot <mhawro200@caledonian.ac.uk>
2020-03-02 01:27:20 +0000

    Delete a booking from the UI and server

--- ionic-angular/src/app/bookings/booking.service.ts
+++ ionic-angular/src/app/bookings/booking.service.ts
@@ -70,13 +70,14 @@ export class BookingService {
     }
 
     cancelBooking(bookingId: string) {
-        return this.bookings.pipe(
+        return this.http.delete(`https://ionic-angular-course-f44b5.firebaseio.com/bookings/${bookingId}.json`).pipe(switchMap(() => {
+            return this.bookings;
+        }),
             take(1),
-            delay(1000),
             tap(bookings => {
-                this._bookings.next(bookings.filter(b => b.id !== bookingId));
-            })
-        );
+            this._bookings.next(bookings.filter(b => b.id !== bookingId));
+        }));
+
     }
 
     fetchBookings() {
