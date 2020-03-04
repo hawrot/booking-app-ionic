5cc2dff5eb4ebbdd03985a123cd34687499fe87c
hawrot <mhawro200@caledonian.ac.uk>
2020-03-04 15:54:51 +0000

    Image picker for desktop browser

--- ionic-angular/src/app/shared/pickers/image-picker/image-picker.component.ts
+++ ionic-angular/src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,35 +1,64 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {CameraResultType, CameraSource, Capacitor, Plugins} from '@capacitor/core';
+import {Platform} from '@ionic/angular';
 
 @Component({
-  selector: 'app-image-picker',
-  templateUrl: './image-picker.component.html',
-  styleUrls: ['./image-picker.component.scss'],
+    selector: 'app-image-picker',
+    templateUrl: './image-picker.component.html',
+    styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent implements OnInit {
+  // @ts-ignore
+  @ViewChild("filePicker") filePicker : ElementRef<HTMLInputElement> ;
   @Output() imagePick = new EventEmitter<string>();
-  selectedImage: string;
+    selectedImage: string;
+    usePicker = false;
 
-  constructor() { }
-
-  ngOnInit() {}
-
-  onPickImage(){
-    if(!Capacitor.isPluginAvailable('Camera')){
-      return;
+    constructor(private platform: Platform) {
     }
-    Plugins.Camera.getPhoto({
-      quality: 50,
-      source: CameraSource.Prompt,
-      correctOrientation: true,
-      height: 320,
-      width: 200,
-      resultType: CameraResultType.DataUrl
-    }).then(image =>{
-      this.selectedImage = image.dataUrl;
-      this.imagePick.emit(image.dataUrl);
-    })
-        .catch(err =>{console.log(err)});
-  }
+
+    ngOnInit() {
+        console.log('Mobile', this.platform.is('mobile'));
+        console.log('Hybrid', this.platform.is('hybrid'));
+        console.log('iOS', this.platform.is('ios'));
+        console.log('Android', this.platform.is('android'));
+        console.log('Desktop', this.platform.is('desktop'));
+        if((this.platform.is('mobile') && !this.platform.is('hybrid')) || this.platform.is('desktop')){
+          this.usePicker = true;
+        }
+    }
+
+    onPickImage() {
+        if (!Capacitor.isPluginAvailable('Camera') || this.usePicker) {
+          this.filePicker.nativeElement.click();
+        }
+        Plugins.Camera.getPhoto({
+            quality: 50,
+            source: CameraSource.Prompt,
+            correctOrientation: true,
+            height: 320,
+            width: 200,
+            resultType: CameraResultType.DataUrl
+        }).then(image => {
+            this.selectedImage = image.dataUrl;
+            this.imagePick.emit(image.dataUrl);
+        })
+            .catch(err => {
+                console.log(err)
+            });
+    }
+
+    onFileChosen(event: Event){
+      const pickedFile = (event.target as HTMLInputElement).files[0];
+      if(!pickedFile){
+        return;
+      }
+      const fr = new FileReader();
+      fr.onload = () => {
+        const dataUrl = fr.result.toString();
+        this.selectedImage = dataUrl;
+      };
+      fr.readAsDataURL(pickedFile);
+    }
 
 }
