da56182d704c4151aa849448e0ee7cae857ee720
hawrot <mhawro200@caledonian.ac.uk>
2020-03-06 15:35:02 +0000

    Fix error with wrong userId in discover.page.ts

--- ionic-angular/src/app/places/discover/discover.page.ts
+++ ionic-angular/src/app/places/discover/discover.page.ts
@@ -45,15 +45,18 @@ export class DiscoverPage implements OnInit, OnDestroy {
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    if (event.detail.value === 'all') {
-      this.relevantPlaces = this.loadedPlaces;
-      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
-    } else {
-      this.relevantPlaces = this.loadedPlaces.filter(
-        place => place.userId !== this.authService.userId
-      );
-      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
-    }
+    this.authService.userId.subscribe(userId =>{
+      if (event.detail.value === 'all') {
+        this.relevantPlaces = this.loadedPlaces;
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      } else {
+        this.relevantPlaces = this.loadedPlaces.filter(
+            place => place.userId !== userId
+        );
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      }
+    })
+
   }
 
   ngOnDestroy() {
