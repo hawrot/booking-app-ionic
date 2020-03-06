9a84f4084c3dad718d461243e7ee178d71fbea8e
hawrot <mhawro200@caledonian.ac.uk>
2020-03-06 15:32:57 +0000

    Using the ID Observable to add a booking with userID

--- ionic-angular/src/app/auth/auth.service.ts
+++ ionic-angular/src/app/auth/auth.service.ts
@@ -1,65 +1,73 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 import {BehaviorSubject} from 'rxjs';
 import {User} from './user.model';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 
 export interface AuthResponseData {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  localId: string;
-  expiresIn: string;
-  registered?: boolean;
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    localId: string;
+    expiresIn: string;
+    registered?: boolean;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  private _user = new BehaviorSubject<User>(null);
-  private _token = new BehaviorSubject<string>(null);
+    private _user = new BehaviorSubject<User>(null);
 
-  get userIsAuthenticated() {
-    return this._user.asObservable().pipe(map(user =>{
-      if(user){
-        //convert to boolean with " !! "
-        return !!user.token;
-      }else {
-        return false;
-      }
 
-    }))
-  }
+    get userIsAuthenticated() {
+        return this._user.asObservable().pipe(map(user => {
+            if (user) {
+                //convert to boolean with " !! "
+                return !!user.token;
+            } else {
+                return false;
+            }
 
-  get userId() {
-    return this._user.asObservable().pipe(map(user => {
-      if(user){
-        return user.id;
-      }else {
-        return null;
-      }
-    }))
-  }
+        }))
+    }
 
-  constructor(private http: HttpClient) {}
+    get userId() {
+        return this._user.asObservable().pipe(map(user => {
+            if (user) {
+                return user.id;
+            } else {
+                return null;
+            }
+        }))
+    }
 
-  signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-        `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
-            environment.firebaseAPIKey
-        }`,
-        { email: email, password: password, returnSecureToken: true }
-    );
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  login(email: string, password: string) {
-    return  this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`, {email: email, password: password });
-  }
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+                environment.firebaseAPIKey
+            }`,
+            {email: email, password: password, returnSecureToken: true}
+        ).pipe(tap(this.setUserData.bind(this)));
+    }
 
-  logout() {
-    this._user.next(null);
-  }
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+            {email: email, password: password})
+            .pipe(tap(this.setUserData.bind(this)));
+    }
+
+    logout() {
+        this._user.next(null);
+    }
+
+    private setUserData(userData: AuthResponseData) {
+        const expirationTime = new Date(new Date().getTime() + +userData.expiresIn * 1000); //expiration time in seconds
+        this._user.next(new User(userData.localId, userData.email, userData.idToken, expirationTime));
+    }
 }
