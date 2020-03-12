21d40468c1371e53a2086bc6bea7d8e1a0d491d7
hawrot <mhawro200@caledonian.ac.uk>
2020-03-12 10:56:31 +0000

    Prevent redirection to the services if not authenticated

--- ionic-angular/src/app/auth/auth.guard.ts
+++ ionic-angular/src/app/auth/auth.guard.ts
@@ -3,6 +3,7 @@ import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from './auth.service';
+import {take, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,11 @@ export class AuthGuard implements CanLoad {
     route: Route,
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.userIsAuthenticated) {
-      this.router.navigateByUrl('/auth');
-    }
-    return this.authService.userIsAuthenticated;
+
+    return this.authService.userIsAuthenticated.pipe(take(1), tap(isAuthenticated =>{
+      if (!isAuthenticated) {
+        this.router.navigateByUrl('/auth');
+      }
+    }));
   }
 }
