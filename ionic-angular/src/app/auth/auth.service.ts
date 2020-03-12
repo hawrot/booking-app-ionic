e01acdb9dd82c5163525b9e882ca8c9db02dae6a
hawrot <mhawro200@caledonian.ac.uk>
2020-03-12 11:18:26 +0000

    Store the auth data in local storage

--- ionic-angular/src/app/auth/auth.service.ts
+++ ionic-angular/src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import {environment} from '../../environments/environment';
 import {BehaviorSubject} from 'rxjs';
 import {User} from './user.model';
 import {map, tap} from 'rxjs/operators';
+import {Plugins} from '@capacitor/core';
 
 export interface AuthResponseData {
     kind: string;
@@ -69,5 +70,11 @@ export class AuthService {
     private setUserData(userData: AuthResponseData) {
         const expirationTime = new Date(new Date().getTime() + +userData.expiresIn * 1000); //expiration time in seconds
         this._user.next(new User(userData.localId, userData.email, userData.idToken, expirationTime));
+        this.storeAuthData(userData.localId, userData.idToken, expirationTime.toISOString());
+    }
+
+    private storeAuthData(userId: string, token: string, tokenExpirationDate: string){
+        const data = JSON.stringify({userId: userId, token: token, tokenExpirationDate: tokenExpirationDate});
+        Plugins.Storage.set({key: 'authData', value: data });
     }
 }
