768e19b63ca80131eb715e2df213c24a6a668d5b
hawrot <mhawro200@caledonian.ac.uk>
2020-03-12 11:34:34 +0000

    Implement local storage and autoLogin method

--- ionic-angular/src/app/auth/auth.guard.ts
+++ ionic-angular/src/app/auth/auth.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 
 import { AuthService } from './auth.service';
-import {take, tap} from 'rxjs/operators';
+import {switchMap, take, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,13 @@ export class AuthGuard implements CanLoad {
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
 
-    return this.authService.userIsAuthenticated.pipe(take(1), tap(isAuthenticated =>{
+    return this.authService.userIsAuthenticated.pipe(take(1), switchMap(isAuthenticated =>{
+      if(!isAuthenticated){
+        return this.authService.autoLogin();
+      }else {
+        return of(isAuthenticated);
+      }
+    }), tap(isAuthenticated =>{
       if (!isAuthenticated) {
         this.router.navigateByUrl('/auth');
       }
--- ionic-angular/src/app/auth/auth.service.ts
+++ ionic-angular/src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, from} from 'rxjs';
 import {User} from './user.model';
 import {map, tap} from 'rxjs/operators';
 import {Plugins} from '@capacitor/core';
+import {strategy} from '@angular-devkit/core/src/experimental/jobs';
 
 export interface AuthResponseData {
     kind: string;
@@ -48,6 +49,31 @@ export class AuthService {
     constructor(private http: HttpClient) {
     }
 
+
+    autoLogin(){
+       return from (Plugins.Storage.get({key: 'authData'})).pipe(map(storedData =>{
+           if(!storedData || !storedData.value){
+               return null;
+           }
+           const parsedData = JSON.parse(storedData.value) as {token: string; tokenExpirationDate: string, userId: string, email: string };
+           const expirationTime = new Date(parsedData.tokenExpirationDate);
+           if(expirationTime <= new Date()){
+               return null;
+           }
+           const user = new User(parsedData.userId, parsedData.email, parsedData.token, expirationTime);
+           return user;
+       }),
+           tap(user =>{
+               if(user){
+                  this._user.next(user);
+               }
+           }),
+           map(user =>{
+               return  !!user;
+           })
+           );
+    }
+
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
             `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
@@ -59,7 +85,7 @@ export class AuthService {
 
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
-            {email: email, password: password})
+            {email: email, password: password, returnSecureToken: true})
             .pipe(tap(this.setUserData.bind(this)));
     }
 
@@ -70,11 +96,11 @@ export class AuthService {
     private setUserData(userData: AuthResponseData) {
         const expirationTime = new Date(new Date().getTime() + +userData.expiresIn * 1000); //expiration time in seconds
         this._user.next(new User(userData.localId, userData.email, userData.idToken, expirationTime));
-        this.storeAuthData(userData.localId, userData.idToken, expirationTime.toISOString());
+        this.storeAuthData(userData.localId, userData.idToken, expirationTime.toISOString(), userData.email);
     }
 
-    private storeAuthData(userId: string, token: string, tokenExpirationDate: string){
-        const data = JSON.stringify({userId: userId, token: token, tokenExpirationDate: tokenExpirationDate});
+    private storeAuthData(userId: string, token: string, tokenExpirationDate: string, email: string){
+        const data = JSON.stringify({userId: userId, token: token, tokenExpirationDate: tokenExpirationDate, email: email });
         Plugins.Storage.set({key: 'authData', value: data });
     }
 }
